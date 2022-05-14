dd5f925bedbc482b602185eb7872f6144ce20476
wuyongqiang <wuyongqiang@trunk.tech>
2022-05-14 23:19:23 +0800

    ADD: 增加方法引用

--- main.cpp
+++ main.cpp
@@ -2,6 +2,9 @@
 #include "day1/code_01_basic.cpp"
 #include "day1/code_02_if.cpp"
 #include "day1/code_03_while.cpp"
+#include "day1/code_04_for.cpp"
+#include "day1/code_05_goto.cpp"
+#include "day1/code_06_array.cpp"
 
 int main() {
 //    day1::func();
@@ -11,5 +14,11 @@ int main() {
 //    day1::func_while();
 //    day1::func_csz();
 //    day1::func_do_while();
-    day1::func_sxh();
+//    day1::func_sxh();
+//    day1::func_for();
+//    day1::func_for_1();
+//    day1::func_for_2();
+//    day1::func_for_3();
+//    day1::func_goto();
+    day1::func_array();
 }
